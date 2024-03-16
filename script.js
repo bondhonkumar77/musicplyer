20c99e7d68f8b16d0f92407b4cd57aa5b1c73519
Bondhon Kumar Das <126702821+bondhonkumar77@users.noreply.github.com>
2024-03-16 11:30:09 +0600

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // Array of song titles and source links
     const songs = [
         { title: 'Song 1', src: 'https://github.com/bondhonkumar77/musicplyer/blob/main/%5BSPOTIFY-DOWNLOADER.COM%5D%20Solstice/Downtown.mp3' },
-        { title: 'Song 2', src: 'https://example.com/song2.mp3' },
+        { title: 'Song 2', src: '[SPOTIFY-DOWNLOADER.COM] Solstice/Downtown.mp3' },
         { title: 'Song 3', src: 'https://example.com/song3.mp3' },
         { title: 'Song 4', src: 'https://example.com/song4.mp3' },
         { title: 'Song 5', src: 'https://example.com/song5.mp3' },
