579d434a5c9247a6461950e441dea40c57daf053
Bondhon Kumar Das <126702821+bondhonkumar77@users.noreply.github.com>
2024-03-16 12:18:55 +0600

    Update solstice.js

--- solstice.js
+++ solstice.js
@@ -7,18 +7,18 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Define an array of songs with their titles, source paths, and thumbnails
     const songs = [
-        { title: 'Downtown', src: './Solstice/Downtown.mp3', thumbnail: './Solstic/Folder.jpg' },
-                { title: 'Downtown', src: './Solstice/Downtown.mp3', thumbnail: './Solstic/Folder.jpg' },
-                { title: 'Flight', src: './Solstice/Flight.mp3', thumbnail: './Solstic/Folder.jpg' },
-                { title: 'Moirse', src: './Solstice/Moires.mp3', thumbnail: './Solstic/Folder.jpg' },
-                { title: 'Riptide', src: './Solstice/Riptide.mp3', thumbnail: './Solstic/Folder.jpg' },
-                { title: 'Satellites', src: './Solstice/Satellites.mp3', thumbnail: './Solstic/Folder.jpg' },
-                { title: 'Snowland', src: './Solstice/Snowland.mp3', thumbnail: './Solstic/Folder.jpg' },
-                { title: 'The Woods', src: './Solstice/The Woods.mp3', thumbnail: './Solstic/Folder.jpg' },
-                { title: 'Ziva', src: './Solstice/Ziva.mp3', thumbnail: './Solstic/Folder.jpg' },
-                { title: 'Safira', src: './Solstice/Safira.mp3', thumbnail: './Solstic/Folder.jpg' },
+       
+                { title: 'Downtown', src: './Solstice/Downtown.mp3', thumbnail: './Solstice/Folder.jpg' },
+                { title: 'Flight', src: './Solstice/Flight.mp3', thumbnail: './Solstice/Folder.jpg' },
+                { title: 'Moirse', src: './Solstice/Moires.mp3', thumbnail: './Solstice/Folder.jpg' },
+                { title: 'Riptide', src: './Solstice/Riptide.mp3', thumbnail: './Solstice/Folder.jpg' },
+                { title: 'Satellites', src: './Solstice/Satellites.mp3', thumbnail: './Solstice/Folder.jpg' },
+                { title: 'Snowland', src: './Solstice/Snowland.mp3', thumbnail: './Solstice/Folder.jpg' },
+                { title: 'The Woods', src: './Solstice/The Woods.mp3', thumbnail: './Solstice/Folder.jpg' },
+                { title: 'Ziva', src: './Solstice/Ziva.mp3', thumbnail: './Solstice/Folder.jpg' },
+                { title: 'Safira', src: './Solstice/Safira.mp3', thumbnail: './Solstice/Folder.jpg' },
         
-        { title: 'Song 2', src: 'path_to_song2.mp3', thumbnail: 'thumbnail2.jpg' },
+        
         // Add more songs as needed
     ];
 
